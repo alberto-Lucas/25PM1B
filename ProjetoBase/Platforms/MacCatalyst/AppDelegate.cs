4dca00486a84e3905e1cd3b26e32a24a3ea34588
alberto-Lucas <lucasalberto.etec@hotmail.com>
2025-02-19 18:11:00 -0300

    ProjetoBase Vazio

---
+++ ProjetoBase/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace ProjetoBase;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
